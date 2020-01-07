c2d22efed28e3988ff6ee40864fc4e6bc3d92926
ZEFANNYA RP <zefanya.pradipta@garasi.id>
2020-01-07 13:43:24 +0700

    added a new page for showing student's details

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layout/main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col-7">
+                <h1 class="mt-3">Detail Mahasiswa</h1>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $student->nama  }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $student->nim }}</h6>
+                            <p class="card-text">{{ $student->email }}</p>
+                            <p class="card-text">{{ $student->jurusan }}</p>
+                            <button type="submit" class="btn btn-primary">edit</button>
+                            <button type="submit" class="btn btn-danger">delete</button>
+                            <a href="/students" class="card-link">Kembali</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+@endsection
